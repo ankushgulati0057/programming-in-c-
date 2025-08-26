a2753d25ca9dc1a47d123c359084b3f8a23eaad2
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-08-27 00:06:37 +0530

    Add files via upload

---
+++ length and width.c
@@ -0,0 +1,18 @@
+/*Experiment 2: Operators
+1. WAP a C program to calculate the area and perimeter of a rectangle based on its
+length and width */
+#include<stdio.h>
+int main() 
+{
+    float length, width, a, p;
+
+    printf("Enter the length of the rectangle");
+    scanf("%f", &length);
+    printf("Enter the width of the rectangle");
+    scanf("%f", &width);
+    a= length * width;
+    p= 2 * (length + width);
+  printf("\nArea of the rectangle = %.2f\n", a);
+    printf("Perimeter of the rectangle = %.2f\n", p);
+    /*return 0;   */
+}
