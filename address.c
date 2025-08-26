a865f6645b298ea34aa69c61e3dc4109a8cb4855
ankushgulati0057 <ankushgulati0057@gmail.com>
2025-08-26 23:41:09 +0530

    Add files via upload

---
+++ address.c
@@ -0,0 +1,13 @@
+/* Experiment 1: Installation, Environment Setup and starting with C language
+ 2.Write a C Program to print the address in multiple lines (new line).*/
+#include <stdio.h>
+int main() 
+{
+    printf("John Doe\n");
+    printf("123, Park Street\n");
+    printf("Near City Mall\n");
+    printf("New Delhi, Delhi - 110001\n");
+    printf("India\n");
+
+    return 0;
+}
